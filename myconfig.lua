93c1a22f7f9ae37878df34486e9f3b91ff62ffe1
ShyngysM <tschingis.muratbek@gmail.com>
2023-03-01 21:02:32 +0100

    added TODOs

--- myconfig.lua
+++ myconfig.lua
@@ -2,13 +2,13 @@ vim.diagnostic.disable()
 vim.opt.number = false
 lvim.transparent_window = true
 
-
+-- TODO: toggle of vim.diagnostic
 --function max(num1, num2)
 ---  if (vim.diagnostic.is_diabled)
 --end
-
-vim.keymap.set('n', '<Leader>e', ':lua vim.diagnostic.enable()<CR>')
-vim.keymap.set('n', '<Leader>d', ':lua vim.diagnostic.disable()<CR>')
--- vim.opt.showtabline = 0
+--vim.keymap.set('n', '<Leader>e', ':lua vim.diagnostic.enable()<CR>')
+--vim.keymap.set('n', '<Leader>d', ':lua vim.diagnostic.disable()<CR>')
+--
 -- lvim.builtin.bufferline.options.always_show_bufferline = false
+-- TODO relative number
 lvim.builtin.nvimtree.active = false
