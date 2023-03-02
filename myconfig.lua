6fc117b69ba01082b724f872d84d7aee9cbbc600
ShyngysM <tschingis.muratbek@gmail.com>
2023-03-02 11:41:14 +0100

    added dummy toggle of diagnostic and VSCode themes

--- myconfig.lua
+++ myconfig.lua
@@ -1,14 +1,13 @@
-vim.diagnostic.disable()
-vim.opt.number = false
-lvim.transparent_window = true
+vim.diagnostic.disable()      --disable diagnostic by default
+vim.opt.relativenumber = true       -- set relativenumber
+lvim.transparent_window = false       -- control transparency
+vim.o.termguicolors = true      --adds more colorschemes
+lvim.colorscheme = "default"      -- set transparency
+-- TODO: toggle of vim.diagnostic properly, this one is dummy with 2 bindings
+lvim.keys.normal_mode["<Leader>t"] = ':lua vim.diagnostic.enable()<CR>'
+lvim.keys.normal_mode["<Leader>z"] = ':lua vim.diagnostic.disable()<CR>'
 
--- TODO: toggle of vim.diagnostic
---function max(num1, num2)
----  if (vim.diagnostic.is_diabled)
---end
---vim.keymap.set('n', '<Leader>e', ':lua vim.diagnostic.enable()<CR>')
---vim.keymap.set('n', '<Leader>d', ':lua vim.diagnostic.disable()<CR>')
---
--- lvim.builtin.bufferline.options.always_show_bufferline = false
--- TODO relative number
-lvim.builtin.nvimtree.active = false
+-- Change the theme:
+lvim.keys.normal_mode["<C-d>"] = ":colorscheme lunar<CR>"
+--VSCODE THEME https://github.com/Mofiqul/vscode.nvim
+lvim.keys.normal_mode["<C-l>"] = ":lua require('vscode').change_style('light')<CR>"
